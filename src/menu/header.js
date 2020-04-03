83bf857e318da6b6d5a71389afa13d8236de834c
liuge36 <893861319@qq.com>
2020-04-03 17:57:21 +0800

    init project

---
+++ src/menu/header.js
@@ -0,0 +1,15 @@
+// 菜单 顶栏
+export default [
+  { path: '/index', title: '首页', icon: 'home' },
+  {
+    title: '页面',
+    icon: 'folder-o',
+    children: [
+      { path: '/page1', title: '页面 1' },
+      { path: '/page2', title: '页面 2' },
+      { path: '/page3', title: '页面 3' },
+      { path: '/page-demo', title: '我的第一个页面-顶栏' } // path的值 要和路由中的 path值 对应上
+
+    ]
+  }
+]
